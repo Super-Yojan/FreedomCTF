02992e10df284fd3a23f22cdd83b76a76c6af320
hypnogod <62040270+hypnogod@users.noreply.github.com>
2020-03-24 19:31:37 -0400

    Update TestLogin.js
    just added the top half

--- templates/TestLogin.js
+++ templates/TestLogin.js
@@ -1,4 +1,67 @@
-  render() 
+import React, { Component } from 'react'
+import '../../css/Login.css';
+import { Link } from 'react-router-dom';
+//these two are something i got online for this.props.history.pussh
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
+
+export default class Login extends Component {
+
+
+  state={
+      TeamName:'',
+      TeamPassword:'',
+      redirect:null,
+  }
+//This handles change in input to get all the necessary information...
+  handelChange = (event) => {
+    let nam = event.target.name;
+    let value = event.target.value;
+    this.setState({
+      [nam] : value
+    })
+  }
+
+
+  //this is useless code i tried to use it to do the redirect but didn't work
+  //the paramater was supposed to be a boolean value , and if the function reutrns
+  //true then it will re-render the whole page.
+  shouldComponentUpdate(should){
+    return should;
+  }
+
+//It handlles submit event
+  onSubmitHandler = (event) => {
+    //this prevents default action by the form submit
+    event.preventDefault();
+    //this the data that we are sendting to the api
+    let data = {
+      TeamName:this.state.TeamName,
+      TeamPassword:this.state.TeamPassword,
+    }
+    //just printing the data for debuging.
+    console.log(data);
+
+    //This is the put call being called
+    axios.put('http://127.0.0.1:5000/login',{data})
+    .then(res => {
+      console.log(res.data);
+      if (res.data.Result == 1){
+
+        //we need to redirect to '/map' but i don't know how to do it..
+        //I serached a lot online but nothing really worked...
+
+        //One of the most possible answer was using 'this.props.history.push('/map')'
+        console.log(this.props.location);
+      }
+    } )
+
+  }
+
+  
+render() 
   {
       return (
           <React.Fragment>
@@ -32,3 +95,4 @@
         )
 
       }
+}
