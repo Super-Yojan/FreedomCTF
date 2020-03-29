2ac3680ca966d9ab52b9c78c3282869801e90497
GryphNdor <griffin@ogurafamily.com>
2020-03-29 18:20:32 -0400

    website css and minor tweaks to navbar/scaling

---
+++ FrontEnd/src/components/pages/Challenges.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Challenges extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>This is a Map, very cool</h1>
+
+            </React.Fragment>
+        )
+    }
+}
+
+
