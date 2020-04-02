74bec219fce29938a6710054c51cf2b7080336b0
Super-Yojan <gautamyojan0@gmail.com>
2020-04-01 20:11:47 -0400

    should work in GCP

--- FrontEnd/src/components/NavMenu.js
+++ FrontEnd/src/components/NavMenu.js
@@ -7,8 +7,9 @@ export default function NavMenu() {
         <React.Fragment>
             <nav className="navbar">
                 {/* <div className = "overlay"></div> <-- dont know what to do with this */}
+                <Link className="navbar-logo">Freedom CyberChase <img className = "navbar-logo-img" src="https://www.lcps.org/cms/lib/VA01000195/Centricity/Template/GlobalAssets/images///logos/FreedomHS-eaglehead177x160.png" alt="Freedom"/></Link>
+
                 <ul className = "navbar-nav">
-                    <Link className="navbar-logo">Freedom CyberChase <img className = "navbar-logo-img" src="https://www.lcps.org/cms/lib/VA01000195/Centricity/Template/GlobalAssets/images///logos/FreedomHS-eaglehead177x160.png" alt="Freedom"/></Link>
                     <Link className="navbar-item" to = '/'> Home </Link> 
                     <Link className="navbar-item" to = '/login'>Login</Link>
                 {/*    <Link to = '/map'>Map</Link> */}
